0ff711ed9dbb9fc2c7beef2b4cf73c4462fcea1a
iamsam786 <samukelondlela2.0@gmail.com>
2025-10-12 01:46:44 +0200

    completed course POJO

---
+++ src/main/java/za/ca/cput/easyenrolclient/domain/Course.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ca.cput.easyenrolclient.domain;
+
+/**
+ *
+ * @author samuk
+ */
+public class Course {
+    private int courseId;
+    private String courseCode;
+    private String courseName;
+}
